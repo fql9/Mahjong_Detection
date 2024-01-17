21d698711d77403ad245839676b9b71e8e50e6e5
FU Qianli <112332388+HulaHulakkr@users.noreply.github.com>
2024-01-18 00:20:24 +0800

    Add files via upload

---
+++ build-Mahjong_Detection-Boot2Qt_6_6_1_Intel_NUC-Debug/CMakeFiles/3.25.2/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Windows-6.2.9200")
+set(CMAKE_HOST_SYSTEM_NAME "Windows")
+set(CMAKE_HOST_SYSTEM_VERSION "6.2.9200")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "AMD64")
+
+include("D:/Code/3.Qt/6.6.1/Boot2Qt/intel-skylake-64/toolchain/sysroots/x86_64-w64-mingw32/usr/lib/cmake/Qt6/qt.toolchain.cmake")
+
+set(CMAKE_SYSTEM "Linux")
+set(CMAKE_SYSTEM_NAME "Linux")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "x86-64-v3")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
---
+++ build-Mahjong_Detection-Boot2Qt_6_6_1_Intel_NUC-Debug/qtcsettings.cmake
@@ -0,0 +1,15 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "C:/Users/fuqianli/OneDrive - City University of Hong Kong - Student/Documents/Project/Mahjong_Detection/build-Mahjong_Detection-Boot2Qt_6_6_1_Intel_NUC-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_SYSROOT" "D:/Code/3.Qt/6.6.1/Boot2Qt/intel-skylake-64/toolchain/sysroots/x86-64-v3-poky-linux" CACHE "PATH" "" FORCE)
+set("CMAKE_C_COMPILER_TARGET" "x86_64-poky-linux" CACHE "STRING" "" FORCE)
+set("CMAKE_CXX_COMPILER_TARGET" "x86_64-poky-linux" CACHE "STRING" "" FORCE)
+set("CMAKE_CXX_COMPILER" "D:/Code/3.Qt/6.6.1/Boot2Qt/intel-skylake-64/toolchain/sysroots/x86_64-w64-mingw32/usr/bin/x86_64-poky-linux/x86_64-poky-linux-g++.exe" CACHE "STRING" "" FORCE)
+set("CMAKE_C_COMPILER" "D:/Code/3.Qt/6.6.1/Boot2Qt/intel-skylake-64/toolchain/sysroots/x86_64-w64-mingw32/usr/bin/x86_64-poky-linux/x86_64-poky-linux-gcc.exe" CACHE "STRING" "" FORCE)
+set("CMAKE_PREFIX_PATH" "D:/Code/3.Qt/6.6.1/Boot2Qt/intel-skylake-64/toolchain/sysroots/x86-64-v3-poky-linux/usr" CACHE "STRING" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "D:/Code/3.Qt/6.6.1/Boot2Qt/intel-skylake-64/toolchain/sysroots/x86_64-w64-mingw32/usr/bin/qmake.exe" CACHE "STRING" "" FORCE)
+set("CMAKE_TOOLCHAIN_FILE" "D:/Code/3.Qt/6.6.1/Boot2Qt/intel-skylake-64/toolchain/sysroots/x86_64-w64-mingw32/usr/lib/cmake/Qt6/qt.toolchain.cmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_MAKE_PROGRAM" "D:/Code/3.Qt/6.6.1/Boot2Qt/intel-skylake-64/toolchain/sysroots/x86_64-w64-mingw32/usr/bin/ninja.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
